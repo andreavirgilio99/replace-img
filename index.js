23c4aa85e43a4448559efd1f27713d3eafa31511
andreavirgilio99 <andrea.virgilio@tsi-srl.it>
2024-04-18 18:38:26 +0200

    ( • )( • )ԅ(‾⌣‾ԅ)

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const fs = require('fs');
+const sharp = require('sharp');
+const qr = require('qrcode');
+
+const app = express();
+const port = 3000;
+const QR_SIZE = 104;
+
+app.get('/genera-qr/:testo', async (req, res) => {
+  try {
+    const testo = req.params.testo;
+
+    const qrDataUri = await qr.toDataURL(testo, { errorCorrectionLevel: 'H', width: QR_SIZE });
+
+    const qrBuffer = Buffer.from(qrDataUri.split(',')[1], 'base64');
+
+    const immagineBianca = await sharp({
+      create: {
+        width: QR_SIZE,
+        height: QR_SIZE,
+        channels: 4,
+        background: { r: 255, g: 255, b: 255, alpha: 1 }
+      }
+    })
+    .png()
+    .toBuffer();
+
+    const qrImageResized = await sharp(qrBuffer)
+      .toBuffer();
+
+    const qrImage = await sharp(immagineBianca)
+      .composite([{ input: qrImageResized, top: 0, left: 0 }])
+      .toBuffer();
+
+    const immagineClonata = Buffer.from(fs.readFileSync('./template_sticker.png'));
+
+    const risultato = await sharp(immagineClonata)
+    .composite([{ input: qrImage, top: 10, left: 177 }])
+    .png()
+    .toBuffer();
+
+    res.set('Content-Type', 'image/png');
+    res.send(risultato);
+  } catch (error) {
+    console.error('Si è verificato un errore durante la generazione del QR code:', error);
+    res.status(500).send('Si è verificato un errore durante la generazione del QR code');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
+});
