1355dc5bc3b87091bbb94633ea8264afb0f3d0d6
andreavirgilio99 <andrea.virgilio@tsigroupsrl.it>
2024-05-03 10:56:06 +0200

    modificata url per aggiungere il tipo, ancora non aggiunta la scritta

--- index.js
+++ index.js
@@ -8,44 +8,51 @@ const app = express();
 const port = 3000;
 const QR_SIZE = 104;
 
-app.get('/genera-qr/:testo', async (req, res) => {
-  try {
-    const testo = req.params.testo;
+app.get('/genera-qr/:tipo/:testo', async (req, res) => {
+  const tipo = req.params.tipo
+  const testo = req.params.testo;
 
-    const qrDataUri = await qr.toDataURL(testo, { errorCorrectionLevel: 'H', width: QR_SIZE });
+  if (tipo !== "pausamatic" && tipo !== "gettoniera") {
+    res.set('Content-Type', 'text/plain');
+    res.send('tipo dispositivo non valido, il tipo deve essere pausamatic oppure gettoniera');
+  } else {
+    try {
+      const qrDataUri = await qr.toDataURL(testo, { errorCorrectionLevel: 'H', width: QR_SIZE });
 
-    const qrBuffer = Buffer.from(qrDataUri.split(',')[1], 'base64');
+      const qrBuffer = Buffer.from(qrDataUri.split(',')[1], 'base64');
 
-    const immagineBianca = await sharp({
-      create: {
-        width: QR_SIZE,
-        height: QR_SIZE,
-        channels: 4,
-        background: { r: 255, g: 255, b: 255, alpha: 1 }
-      }
-    })
-      .png()
-      .toBuffer();
+      const immagineBianca = await sharp({
+        create: {
+          width: QR_SIZE,
+          height: QR_SIZE,
+          channels: 4,
+          background: { r: 255, g: 255, b: 255, alpha: 1 }
+        }
+      })
+        .png()
+        .toBuffer();
 
-    const qrImageResized = await sharp(qrBuffer)
-      .toBuffer();
+      const qrImageResized = await sharp(qrBuffer)
+        .toBuffer();
 
-    const qrImage = await sharp(immagineBianca)
-      .composite([{ input: qrImageResized, top: 0, left: 0 }])
-      .toBuffer();
+      const qrImage = await sharp(immagineBianca)
+        .composite([{ input: qrImageResized, top: 0, left: 0 }])
+        .toBuffer();
 
-    const immagineClonata = Buffer.from(fs.readFileSync(path.join(__dirname, 'template_sticker.PNG')));
+      const immagineClonata = Buffer.from(fs.readFileSync(path.join(__dirname, 'template_sticker.PNG')));
 
-    const risultato = await sharp(immagineClonata)
-      .composite([{ input: qrImage, top: 10, left: 177 }])
-      .png()
-      .toBuffer();
+      const risultato = await sharp(immagineClonata)
+        .composite([{ input: qrImage, top: 10, left: 177 }])
+        .png()
+        .toBuffer();
 
-    res.set('Content-Type', 'image/png');
-    res.send(risultato);
-  } catch (error) {
-    console.error('Si è verificato un errore durante la generazione del QR code:', error);
-    res.status(500).send('Si è verificato un errore durante la generazione del QR code');
+      res.set('Content-Type', 'image/png');
+      res.send(risultato);
+
+    } catch (error) {
+      console.error('Si è verificato un errore durante la generazione del QR code:', error);
+      res.status(500).send('Si è verificato un errore durante la generazione del QR code');
+    }
   }
 });
 
